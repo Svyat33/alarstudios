cc3c523f3aa5810f3b5f8c5e7e9b60a49a433c3b
Svyat <svyatka@gmail.com>
2021-07-19 11:29:29 +0300

    дополнил ридми, добавил рестарт вконце инсталла скрипта.

--- install.sh
+++ install.sh
@@ -2,17 +2,18 @@
 set -e
 echo "Start containers"
 docker-compose up -d > /dev/null 2>&1
-sleep 5
+sleep 10
 echo "start cunsumers"
 docker-compose up -d > /dev/null 2>&1
 sleep 3
 echo "Create database tables"
-docker-compose exec account python manage.py migrate > /dev/null 2>&1
-docker-compose exec dots python manage.py migrate > /dev/null 2>&1
+docker-compose exec account python manage.py migrate
+docker-compose exec dots python manage.py migrate
 echo "Create test users"
 docker-compose exec account python manage.py create_users
 echo "Generate 500000 dots"
 docker-compose exec dots python manage.py populate
 echo "Copy dots to redis DB"
 docker-compose exec dots python manage.py dottoredis
-date
+echo "restart docker"
+docker-compose restart
